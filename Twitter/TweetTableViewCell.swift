2eea8d7fde6f98d9fe7229b88d4059f11fb24f21
law-lin <lawrencelin101@gmail.com>
2021-03-02 04:21:06 -0500

    Complete Twitter app

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Lawrence Lin on 3/2/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
